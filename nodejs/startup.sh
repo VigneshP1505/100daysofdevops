1215e9a74199c6d0bb9f6eb59c8841b74356db4d
Vignesh Pugazhendhi <>
2025-11-09 17:42:10 -0500

    nodejs pm2

--- nodejs/startup.sh
+++ nodejs/startup.sh
@@ -3,6 +3,9 @@
 if [ "$NODE_ENV" = "production" ]; then
     echo "Starting in production mode"
     yarn start
+elif [ "$NODE_ENV" = "pm2" ]; then
+    echo "staring pm2 mode"
+    yarn start:prod
 else
     echo "Starting in dev mode"
     yarn dev
